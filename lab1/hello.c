dd618a19297db2eef3a094b7ff8083510caae3cd
cuijxin <cuijxin@126.com>
2018-12-09 10:32:26 +0800

    hello world

---
+++ lab1/hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Description: hello.c
+	> Author: lkong
+	> Mail: lkong@tencent.com 
+	> Created Time: 2018-12-09 10:29:30
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("Hello world!\n");
+}
